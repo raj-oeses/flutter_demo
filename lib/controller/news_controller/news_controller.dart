af5a973a869fb528f38fabb8c8dd09ecaedd44ac
ashish rajbanshi <ashishrajbanshi40@gmail.com>
2022-07-12 08:31:31 +0545

    updated

--- lib/controller/news_controller/news_controller.dart
+++ lib/controller/news_controller/news_controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_tests/models/news_model.dart';
 import 'package:flutter_tests/services/news_services.dart';
 import 'package:get/get.dart';
+
 class NewsController extends GetxController {
   var fetchedData = NewsModel().obs;
   var isLoading = true.obs;
@@ -18,7 +19,8 @@ class NewsController extends GetxController {
       var data = await NewsServices.getNews();
       if (data != null) {
         isLoading.value = false;
-        fetchedData.value = data;
+        data.fold((data) => fetchedData.value = data,
+            (error) => debugPrint('the error is $error'));
       }
     } on Exception catch (e) {
       debugPrint('there is an exception $e');
--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_tests/models/news_model.dart';
 import 'package:flutter_tests/utilities/date_time_parser.dart';
@@ -8,7 +9,7 @@ import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 class NewsServices {
-  static Future<NewsModel?> getNews() async {
+  static Future<Either<NewsModel,String>?> getNews() async {
     String newsUrl = 'https://newsapi.org/v2/everything?q=tesla&from=${DateFormatter.dateFormat(DateTime.now())}&sortBy=publishedAt&apiKey=1003e994eb4342079d6d359506836f86';
 
     NewsModel newsModel = NewsModel();
@@ -22,11 +23,14 @@ class NewsServices {
         if (response.statusCode == 200) {
           debugPrint('data of plans and vision is ${response.body}');
           newsModel = newsModelFromJson(response.body);
+          return Left(newsModel);
+          // return Right('Error');
         } else {
           debugPrint('status code is ${response.statusCode}');
           newsModel = newsModelFromJson(response.body);
+          return Right('Error');
         }
-        return newsModel;
+        // return Left(newsModel);
       } on Exception catch (e) {
         debugPrint('exception is $e}');
         // TODO
@@ -37,3 +41,33 @@ class NewsServices {
     }
   }
 }
+// class NewsServices {
+//   static Future<NewsModel?> getNews() async {
+//     String newsUrl = 'https://newsapi.org/v2/everything?q=tesla&from=${DateFormatter.dateFormat(DateTime.now())}&sortBy=publishedAt&apiKey=1003e994eb4342079d6d359506836f86';
+//
+//     NewsModel newsModel = NewsModel();
+//     if (await Utilities.isInternetWorking()) {
+//       try {
+//         var headers = {
+//           HttpHeaders.contentTypeHeader: 'application/json',
+//         };
+//         debugPrint('using Url $newsUrl');
+//         var response = await http.get(Uri.parse(newsUrl), headers: headers);
+//         if (response.statusCode == 200) {
+//           debugPrint('data of plans and vision is ${response.body}');
+//           newsModel = newsModelFromJson(response.body);
+//         } else {
+//           debugPrint('status code is ${response.statusCode}');
+//           newsModel = newsModelFromJson(response.body);
+//         }
+//         return newsModel;
+//       } on Exception catch (e) {
+//         debugPrint('exception is $e}');
+//         // TODO
+//       }
+//     }
+//     else {
+//       Get.snackbar('Error', 'No Internet');
+//     }
+//   }
+// }
